b7e275170f7bcc9550124df57fb2554feb3631aa
gurnek1911-rgb <gurnek1911@gmail.com>
2026-04-24 20:45:55 +0530

    Update index.js

--- pages/Admin/index.js
+++ pages/Admin/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { auth, db } from "../../firebase";
+import { auth, db } from "../../lib/firebase";
 import { onAuthStateChanged } from "firebase/auth";
 import {
   collection,
